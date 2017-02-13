7707b849052a8e54cf8371b8be0f4f21a33332fe
hymnah <jaigcasan@gmail.com>
2017-02-13 15:04:21 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3001,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '172.30.123.77',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -128,8 +128,7 @@ io.on('connection', function(socket){
 
 
 
-
-server.listen(port, ip);
+app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
 
 module.exports = app ;

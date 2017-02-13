0ea8b933c7441415a8564e7d6a7f7130a6819967
hymnah <jaigcasan@gmail.com>
2017-02-13 15:56:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3001,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP,
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
